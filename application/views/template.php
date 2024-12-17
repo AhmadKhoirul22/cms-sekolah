a6c8b45c2f2c2fc7a6068c58f5d4931ae1d6e7bd
AhmadKhoirul22 <slowmohuda@gmail.com>
2024-12-17 20:36:05 +0700

    Update template.php

---
+++ application/views/template.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+	<?php include('layout/_css.php') ?>
+	<style>
+		.service-img {
+    height: 200px; /* Atur tinggi yang sama untuk semua gambar */
+    overflow: hidden;
+    position: relative;
+}
+
+.service-img img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover; /* Memastikan gambar tetap rapi */
+    object-position: center; /* Gambar tetap di tengah */
+}
+
+	</style>
+    </head>
+    <body>
+        <!-- Navbar & Hero Start -->
+        <div class="container-fluid position-relative p-0">
+           <?php include('layout/_navbar.php') ?>
+            <!-- Carousel Start -->
+            <?php include('layout/_carousel.php') ?>
+            <!-- Carousel End -->
+        </div>
+        <!-- Navbar & Hero End -->
+		<!-- Features Start -->
+        <?php include('layout/_features.php') ?>
+        <!-- Features End -->
+        <!-- Services Start -->
+        
+        <!-- Services End -->
+
+        <!-- Footer Start -->
+       <?php include('layout/_footer.php') ?>
+        <!-- Footer End -->
+        
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-primary btn-lg-square rounded-circle back-to-top"><i class="fa fa-arrow-up"></i></a>   
+
+        
+        <!-- JavaScript Libraries -->
+        <?php include('layout/_js.php') ?>
+    </body>
+
+</html>
