4e4a934b2775ce3c562c07f943896ae6e64b68fb
AhmadKhoirul22 <slowmohuda@gmail.com>
2024-12-18 10:47:45 +0700

    Update _carousel.php

--- application/views/layout/_carousel.php
+++ application/views/layout/_carousel.php
@@ -7,21 +7,21 @@
                                 <div class="col-lg-0 col-xl-5"></div>
                                 <div class="col-xl-7 animated fadeInLeft">
                                     <div class="text-sm-center text-md-end">
-                                        <h4 class="text-primary text-uppercase fw-bold mb-4"><?= $profile->nama_profile ?></h4>
-                                        <h1 class="display-4 text-uppercase text-white mb-4"><?= $profile->keterangan_profile ?></h1>
-                                        <p class="mb-5 fs-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy... 
+                                        <h5 class="text-primary fw-bold mb-4"><?= $profile->nama_profile ?></h5>
+                                        <!-- <h6 class="display-4 text-white mb-4"><?= $profile->keterangan_profile ?></h6> -->
+                                        <p class="mb-5 fs-5"> <?= $profile->keterangan_profile ?> 
                                         </p>
                                         <div class="d-flex justify-content-center justify-content-md-end flex-shrink-0 mb-4">
-                                            <a class="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2" href="#"><i class="fas fa-play-circle me-2"></i> Watch Video</a>
-                                            <a class="btn btn-primary rounded-pill py-3 px-4 px-md-5 ms-2" href="#">Learn More</a>
+                                            <!-- <a class="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2" href="#"><i class="fas fa-play-circle me-2"></i> Watch Video</a>
+                                            <a class="btn btn-primary rounded-pill py-3 px-4 px-md-5 ms-2" href="#">Learn More</a> -->
                                         </div>
                                         <div class="d-flex align-items-center justify-content-center justify-content-md-end">
                                             <h2 class="text-white me-2">Follow Us:</h2>
                                             <div class="d-flex justify-content-end ms-2">
                                                 <a class="btn btn-md-square btn-light rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
-                                                <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-twitter"></i></a>
+                                                <!-- <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-twitter"></i></a> -->
                                                 <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-instagram"></i></a>
-                                                <a class="btn btn-md-square btn-light rounded-circle ms-2" href=""><i class="fab fa-linkedin-in"></i></a>
+                                                <a class="btn btn-md-square btn-light rounded-circle ms-2" href=""><i class="fab fa-youtube"></i></a>
                                             </div>
                                         </div>
                                     </div>
@@ -38,20 +38,20 @@
                                 <div class="col-12 animated fadeInUp">
                                     <div class="text-center">
                                         <h4 class="text-primary text-uppercase fw-bold mb-4"><?= $profile->nama_profile ?></h4>
-                                        <h1 class="display-4 text-uppercase text-white mb-4"><?= $profile->keterangan_profile ?></h1>
-                                        <p class="mb-5 fs-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy... 
+                                        <!-- <h1 class="display-4 text-uppercase text-white mb-4"><?= $profile->keterangan_profile ?></h1> -->
+                                        <p class="mb-5 fs-5"><?= $profile->keterangan_profile ?> 
                                         </p>
                                         <div class="d-flex justify-content-center flex-shrink-0 mb-4">
-                                            <a class="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2" href="#"><i class="fas fa-play-circle me-2"></i> Watch Video</a>
-                                            <a class="btn btn-primary rounded-pill py-3 px-4 px-md-5 ms-2" href="#">Learn More</a>
+                                            <!-- <a class="btn btn-light rounded-pill py-3 px-4 px-md-5 me-2" href="#"><i class="fas fa-play-circle me-2"></i> Watch Video</a>
+                                            <a class="btn btn-primary rounded-pill py-3 px-4 px-md-5 ms-2" href="#">Learn More</a> -->
                                         </div>
                                         <div class="d-flex align-items-center justify-content-center">
                                             <h2 class="text-white me-2">Follow Us:</h2>
                                             <div class="d-flex justify-content-end ms-2">
                                                 <a class="btn btn-md-square btn-light rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
-                                                <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-twitter"></i></a>
+                                                <!-- <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-twitter"></i></a> -->
                                                 <a class="btn btn-md-square btn-light rounded-circle mx-2" href=""><i class="fab fa-instagram"></i></a>
-                                                <a class="btn btn-md-square btn-light rounded-circle ms-2" href=""><i class="fab fa-linkedin-in"></i></a>
+                                                <a class="btn btn-md-square btn-light rounded-circle ms-2" href=""><i class="fab fa-youtube"></i></a>
                                             </div>
                                         </div>
                                     </div>
