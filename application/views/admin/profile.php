5f53116b50d0313fe7de843e6be3af619140acb8
AhmadKhoirul22 <slowmohuda@gmail.com>
2024-12-18 10:46:40 +0700

    Update profile.php

--- application/views/admin/profile.php
+++ application/views/admin/profile.php
@@ -37,22 +37,72 @@
                 <section class="section">
                     <div class="card">
                         <div class="card-body">
-                            <div id="autohide">
-                            	<?= $this->session->flashdata('alert'); ?>
-                            </div>
+						<div id="autohide">
+							<!-- <?= $this->session->flashdata('alert'); ?> -->
+							<?php if ($this->session->flashdata('alert') == 'warning'): ?>
+							<script>
+								Swal.fire({
+									icon: 'error',
+									title: 'Data tidak valid',
+									text: 'error.',
+									confirmButtonText: 'OK',
+									timer: 3000
+								});
+							</script>
+							<?php elseif ($this->session->flashdata('alert') == 'add'): ?>
+							<script>
+								Swal.fire({
+									icon: 'success',
+									title: 'Berhasil',
+									text: 'Data berhasil ditambahkan!',
+									confirmButtonText: 'OK',
+									timer: 3000
+								});
+							</script>
+							<?php elseif ($this->session->flashdata('alert') == 'delete'): ?>
+							<script>
+								Swal.fire({
+									icon: 'success',
+									title: 'Berhasil',
+									text: 'Data berhasil dihapus!',
+									confirmButtonText: 'OK',
+									timer: 3000
+								});
+							</script>
+							<?php elseif ($this->session->flashdata('alert') == 'update'): ?>
+							<script>
+								Swal.fire({
+									icon: 'success',
+									title: 'Berhasil',
+									text: 'Data berhasil diupdate!',
+									confirmButtonText: 'OK',
+									timer: 3000
+								});
+							</script>
+							<?php endif; ?>
+
+						</div>
 							<form action="<?= base_url('admin/profile/update') ?>" method="post" >
 								<input type="hidden" name="id_profile" value="<?= $profile->id_profile ?>" >
 								<div class="mb-3">
-								<label for="" class="form-label">Nama Profile</label>
+								<label for="" class="form-label">Nama </label>
 								<input type="text" name="nama_profile" value="<?= $profile->nama_profile ?>" class="form-control">
 								</div>
 								<div class="mb-3">
-								<label for="" class="form-label">Alamat Profile</label>
+								<label for="" class="form-label">Alamat </label>
 								<input type="text" name="alamat_profile" value="<?= $profile->alamat_profile ?>" class="form-control">
 								</div>
 								<div class="mb-3">
-								<label for="" class="form-label">Keterangan Profile</label>
-								<textarea name="keterangan_profile" class="form-control" id="dark"><?= $profile->keterangan_profile ?></textarea>
+								<label for="" class="form-label">Email Profile</label>
+								<input type="email" name="email_profile" value="<?= $profile->email_profile ?>" class="form-control">
+								</div>
+								<div class="mb-3">
+								<label for="" class="form-label">No Telp</label>
+								<input type="text" name="telp_profile" value="<?= $profile->telp_profile ?>" class="form-control">
+								</div>
+								<div class="mb-3">
+								<label for="" class="form-label">Keterangan </label>
+								<textarea name="keterangan_profile" class="form-control" 	><?= $profile->keterangan_profile ?></textarea>
 								</div>
 								<div class="mb-3">
 								<button class="btn btn-info" type="submit" >Update</button>
