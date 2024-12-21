80f8e9703412a693f94b498e40fbcf8d04eba1cf
AhmadKhoirul22 <slowmohuda@gmail.com>
2024-12-21 16:28:31 +0700

    Update Dashboard.php

--- application/controllers/Admin/Dashboard.php
+++ application/controllers/Admin/Dashboard.php
@@ -1,5 +1,11 @@
 <?php 
 class Dashboard extends CI_Controller{
+	public function __construct(){
+		parent::__construct();
+		if($this->session->userdata('level') == null){
+			redirect('auth');
+		}
+	}
 	public function index(){
 		$data['title'] = 'Dashboard';
 		$this->load->view('admin/dashboard',$data);
