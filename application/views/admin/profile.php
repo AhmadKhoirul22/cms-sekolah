641aa8cd79691301e593cc11b4ea2a4624eb76dd
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-01-21 15:15:19 +0700

    Update profile.php

--- application/views/admin/profile.php
+++ application/views/admin/profile.php
@@ -82,30 +82,42 @@
 							<?php endif; ?>
 
 						</div>
-							<form action="<?= base_url('admin/profile/update') ?>" method="post" >
-								<input type="hidden" name="id_profile" value="<?= $profile->id_profile ?>" >
+							<form action="<?= base_url('admin/profile/update') ?>" method="post">
+								<input type="hidden" name="id_profile" value="<?= $profile->id_profile ?>">
 								<div class="mb-3">
-								<label for="" class="form-label">Nama </label>
-								<input type="text" name="nama_profile" value="<?= $profile->nama_profile ?>" class="form-control">
+									<label for="" class="form-label">Nama </label>
+									<input type="text" name="nama_profile" value="<?= $profile->nama_profile ?>" class="form-control">
 								</div>
 								<div class="mb-3">
-								<label for="" class="form-label">Alamat </label>
-								<input type="text" name="alamat_profile" value="<?= $profile->alamat_profile ?>" class="form-control">
+									<label for="" class="form-label">Alamat </label>
+									<input type="text" name="alamat_profile" value="<?= $profile->alamat_profile ?>" class="form-control">
 								</div>
 								<div class="mb-3">
-								<label for="" class="form-label">Email Profile</label>
-								<input type="email" name="email_profile" value="<?= $profile->email_profile ?>" class="form-control">
+									<label for="" class="form-label">Email</label>
+									<input type="email" name="email_profile" value="<?= $profile->email_profile ?>" class="form-control">
 								</div>
 								<div class="mb-3">
-								<label for="" class="form-label">No Telp</label>
-								<input type="text" name="telp_profile" value="<?= $profile->telp_profile ?>" class="form-control">
+									<label for="" class="form-label">No Telp</label>
+									<input type="text" name="telp_profile" value="<?= $profile->telp_profile ?>" class="form-control">
 								</div>
 								<div class="mb-3">
-								<label for="" class="form-label">Keterangan </label>
-								<textarea name="keterangan_profile" class="form-control" 	><?= $profile->keterangan_profile ?></textarea>
+									<label for="" class="form-label">Link Youtube</label>
+									<input type="text" name="youtube" value="<?= $profile->youtube ?>" class="form-control">
 								</div>
 								<div class="mb-3">
-								<button class="btn btn-info" type="submit" >Update</button>
+									<label for="" class="form-label">Link Facebook</label>
+									<input type="text" name="facebook" value="<?= $profile->facebook ?>" class="form-control">
+								</div>
+								<div class="mb-3">
+									<label for="" class="form-label">Link Instagram</label>
+									<input type="text" name="instagram" value="<?= $profile->instagram ?>" class="form-control">
+								</div>
+								<div class="mb-3">
+									<label for="" class="form-label">Keterangan </label>
+									<textarea name="keterangan_profile" class="form-control"><?= $profile->keterangan_profile ?></textarea>
+								</div>
+								<div class="mb-3">
+									<button class="btn btn-info" type="submit">Update</button>
 								</div>
 							</form>
                         </div>
