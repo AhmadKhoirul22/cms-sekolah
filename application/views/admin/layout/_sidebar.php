cd0ad06f1713aa5597909092759904edc3eea899
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-02-20 13:49:35 +0700

    Update _sidebar.php

--- application/views/admin/layout/_sidebar.php
+++ application/views/admin/layout/_sidebar.php
@@ -13,45 +13,45 @@
                 <div class="sidebar-menu">
                     <ul class="menu">
                         <li class="sidebar-title">Menu</li>
-<?php $menu = $this->uri->segment(2) ?>
+                        <?php $menu = $this->uri->segment(2) ?>
                         <li class="sidebar-item <?php if($menu=='dashboard'){echo 'active';} ?>">
                             <a href="<?= base_url('admin/dashboard') ?>" class='sidebar-link'>
-                                <i class="bi bi-grid-fill"></i>
+                                <i class="bi bi-house-door"></i>
                                 <span>Dashboard</span>
                             </a>
                         </li>
 
                         <li class="sidebar-item <?php if($menu=='kategori'){echo 'active';}?>">
                             <a href="<?= base_url('admin/kategori') ?>" class='sidebar-link'>
-                                <i class="bi bi-stack"></i>
+                                <i class="bi bi-tags-fill"></i>
                                 <span>Kategori Konten</span>
                             </a>
                         </li>
 
                         <li class="sidebar-item <?php if($menu=='konten'){echo 'active';}?>">
                             <a href="<?= base_url('admin/konten') ?>" class='sidebar-link'>
-                                <i class="bi bi-collection-fill"></i>
+                                <i class="bi bi-card-image"></i>
                                 <span>Konten</span>
                             </a>
                         </li>
 
                         <li class="sidebar-item <?php if($menu=='user'){echo 'active';} ?>">
                             <a href="<?= base_url('admin/user') ?>" class='sidebar-link'>
-                                <i class="bi bi-people"></i>
+                                <i class="bi bi-person-circle"></i>
                                 <span>User</span>
                             </a>
                         </li>
 
 						<li class="sidebar-item <?php if($menu=='profile'){echo 'active';} ?>">
                             <a href="<?= base_url('admin/profile') ?>" class='sidebar-link'>
-                                <i class="bi bi-book"></i>
+                                <i class="bi bi-building-gear"></i>
                                 <span>Profile Sekolah</span>
                             </a>
                         </li>
 
 						<li class="sidebar-item <?php if($menu=='guru'){echo 'active';} ?>">
                             <a href="<?= base_url('admin/guru') ?>" class='sidebar-link'>
-                                <i class="bi bi-people"></i>
+                                <i class="bi bi-mortarboard"></i>
                                 <span>Guru</span>
                             </a>
                         </li>
@@ -59,7 +59,7 @@
 						<li class="sidebar-title">User</li>
 						<li class="sidebar-item <?php if($menu=='logout'){echo 'active';} ?>">
                             <a href="<?= base_url('auth/logout') ?>" onclick="return confirm('yakin untuk logout')" class='sidebar-link'>
-                                <i class="bi bi-people"></i>
+                                <i class="bi bi-box-arrow-left"></i>
                                 <span>Logout</span>
                             </a>
                         </li>
