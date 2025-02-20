b189d1b93cbd7f8df765a54275d3ff32069d150e
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-02-20 13:49:04 +0700

    Update _js.php

--- application/views/admin/layout/_js.php
+++ application/views/admin/layout/_js.php
@@ -1,50 +1,56 @@
 <script src="<?= base_url('assets/mazer/dist/') ?>assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="<?= base_url('assets/mazer/dist/') ?>assets/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets/mazer/dist/') ?>assets/js/bootstrap.bundle.min.js"></script>
 
-    <script src="<?= base_url('assets/mazer/dist/') ?>assets/vendors/simple-datatables/simple-datatables.js"></script>
-    <script>
-        // Simple Datatable
-        let table1 = document.querySelector('#table1');
-        let dataTable = new simpleDatatables.DataTable(table1);
-    </script>
-
-    <script src="<?= base_url('assets/mazer/dist/') ?>assets/js/main.js"></script>
-	<script>
-		$('#autohide').delay('slow').slideDown('slow').delay('5000').slideUp('600');
-	</script>
-	<!--kode editor -->
-	<script src="<?= base_url('assets/mazer/dist/') ?>assets/vendors/tinymce/tinymce.min.js"></script>
-    <script src="<?= base_url('assets/mazer/dist/') ?>assets/vendors/tinymce/plugins/code/plugin.min.js"></script>
-    <script>
-        tinymce.init({ selector: '#textarea' });
-        tinymce.init({ selector: '#dark', toolbar: 'undo redo styleselect bold italic alignleft aligncenter alignright bullist numlist outdent indent code', plugins: 'code' });
-    </script>
+<script src="<?= base_url('assets/mazer/dist/') ?>assets/vendors/simple-datatables/simple-datatables.js"></script>
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        // Pilih semua tombol dengan class "delete-button"
-        const deleteButtons = document.querySelectorAll('.delete-button');
+	// Simple Datatable
+	let table1 = document.querySelector('#table1');
+	let dataTable = new simpleDatatables.DataTable(table1);
+</script>
 
-        deleteButtons.forEach(button => {
-            button.addEventListener('click', function (e) {
-                e.preventDefault(); // Mencegah tautan langsung dijalankan
-                const href = this.getAttribute('data-href'); // Ambil URL dari atribut data-href
+<script src="<?= base_url('assets/mazer/dist/') ?>assets/js/main.js"></script>
+<script>
+	$('#autohide').delay('slow').slideDown('slow').delay('5000').slideUp('600');
+</script>
+<!--kode editor -->
+<script src="<?= base_url('assets/mazer/dist/') ?>assets/vendors/tinymce/tinymce.min.js"></script>
+<script src="<?= base_url('assets/mazer/dist/') ?>assets/vendors/tinymce/plugins/code/plugin.min.js"></script>
+<script>
+	tinymce.init({
+		selector: '#textarea'
+	});
+	tinymce.init({
+		selector: '#dark',
+		toolbar: 'undo redo styleselect bold italic alignleft aligncenter alignright bullist numlist outdent indent code',
+		plugins: 'code'
+	});
+</script>
+<script>
+	document.addEventListener('DOMContentLoaded', function () {
+		// Pilih semua tombol dengan class "delete-button"
+		const deleteButtons = document.querySelectorAll('.delete-button');
 
-                Swal.fire({
-                    title: 'Yakin ingin menghapus?',
-                    text: "Data yang dihapus tidak dapat dikembalikan!",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Ya, hapus!',
-                    cancelButtonText: 'Batal'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        // Redirect ke URL jika dikonfirmasi
-                        window.location.href = href;
-                    }
-                });
-            });
-        });
-    });
+		deleteButtons.forEach(button => {
+			button.addEventListener('click', function (e) {
+				e.preventDefault(); // Mencegah tautan langsung dijalankan
+				const href = this.getAttribute('data-href'); // Ambil URL dari atribut data-href
+
+				Swal.fire({
+					title: 'Yakin ingin menghapus?',
+					text: "Data yang dihapus tidak dapat dikembalikan!",
+					icon: 'warning',
+					showCancelButton: true,
+					confirmButtonColor: '#3085d6',
+					cancelButtonColor: '#d33',
+					confirmButtonText: 'Ya, hapus!',
+					cancelButtonText: 'Batal'
+				}).then((result) => {
+					if (result.isConfirmed) {
+						// Redirect ke URL jika dikonfirmasi
+						window.location.href = href;
+					}
+				});
+			});
+		});
+	});
 </script>
