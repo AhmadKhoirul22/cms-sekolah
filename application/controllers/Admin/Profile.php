6ed03a059c568dd8575128bfd95fc2f4911a67fc
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-02-21 13:23:49 +0700

    Update Profile.php

--- application/controllers/Admin/Profile.php
+++ application/controllers/Admin/Profile.php
@@ -3,13 +3,14 @@ class Profile extends CI_Controller{
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('Alert_model');
+		$this->load->library('Template');
 	}
 	public function index(){
 		$data['title'] = 'Profile';
 		$this->db->from('profile');
 		$data['profile'] = $this->db->get()->row();
 
-		$this->load->view('admin/profile',$data);
+		$this->template->load('admin/template','admin/profile',$data);
 	}
 	public function update(){
 		$data = array(
@@ -18,10 +19,14 @@ class Profile extends CI_Controller{
 			'keterangan_profile' => $this->input->post('keterangan_profile'),
 			'email_profile' => $this->input->post('email_profile'),
 			'telp_profile' => $this->input->post('telp_profile'),
+			'youtube' => $this->input->post('youtube'),
+			'facebook' => $this->input->post('facebook'),
+			'instagram' => $this->input->post('instagram'),
 		);
 		$where = array('id_profile' => $this->input->post('id_profile'));
 		$this->db->update('profile',$data,$where);
 		$alert = $this->Alert_model->update();
+		
 		$this->session->set_flashdata('alert','update');
 		redirect($_SERVER['HTTP_REFERER']);
 	}
