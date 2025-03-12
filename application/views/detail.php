61df822cc6b0cb214a4741713a6b9dd69a1a8205
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-03-12 09:48:44 +0700

    Update detail.php

--- application/views/detail.php
+++ application/views/detail.php
@@ -27,36 +27,43 @@
 					<article
 						style="position: relative;z-index: 2; background: white; padding: 20px; margin-top: -50px; border-radius: 10px;
 						width: 90%;left: 5%;box-shadow: black;">
-						<p class="mb-3" ><span class="bg-success text-light" ><?= $konten['nama_kategori'] ?></span></p>
-						<h1 class="bold mb-3"><?= $konten['judul'] ?></h1>
+						<h3 class="mb-3" ><span class="badge bg-success text-light" ><?= $konten['nama_kategori'] ?></span></h3>
+						<h1 class="bold mb-3" style="font-weight: bold;text-transform: uppercase;" ><?= $konten['judul'] ?></h1>
 						<div class="col-3">
 							<div class="row">
 								<div class="col-sm-3">
 									<i class="fas fa-user fa-3x"></i>
 								</div>
 								<div class="col-sm-9">
-									<p><?= $konten['nama'] ?>
-									 </p>
-									 <p>
-									<?= tanggal_indo($konten['tanggal']) ?>
-									 </p>
+									<div class="col-lg-12">
+										<?=$konten['nama']  ?>
+									</div>
+									<div class="col-lg-12">
+										<?= tanggal_indo($konten['tanggal']) ?>
+									</div>
+									<!-- <p><?= $konten['nama'] ?></p>
+									 <p><?= tanggal_indo($konten['tanggal']) ?></p> -->
 								</div>
 							</div>
 						</div>
 						<article>
 						<p class="mb-5" ><?= $konten['keterangan'] ?></p>
 						</article>
-						<div class="col-3">
+						<div class="col-4">
 							<div class="mb-5">
 								<h3 class="bold">Bagikan</h3>
-								<div class="row">
+								<div class="d-flex">
+								<input id="url-to-copy" type="text" readonly value="<?= current_url() ?>" class="form-control" style="width: 80%;" >
+								<button type="button" class="btn btn-success" style="width: 20%;" onclick="copyToClipboard()">Salin</button>
+								</div>
+								<!-- <div class="row">
 									<div class="col-sm-10 mb-3">
 										<input id="url-to-copy" type="text" readonly value="<?= current_url() ?>" class="form-control">
 									</div>
 									<div class="col-sm-2">
 										<button type="button" class="btn btn-success" onclick="copyToClipboard()">Salin</button>
 									</div>
-								</div>
+								</div> -->
 							</div>
 
 							<script>
