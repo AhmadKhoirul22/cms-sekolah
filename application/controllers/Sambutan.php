85ffbc7719615a3aaadd21bae485da7ec96ccdde
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-05-12 20:39:20 +0700

    Update Sambutan.php

--- application/controllers/Sambutan.php
+++ application/controllers/Sambutan.php
@@ -1,12 +1,14 @@
 <?php 
 class Sambutan extends CI_Controller{
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('Template');
+		$this->load->model('Kategori_model');
+	}
 	public function index(){
 		$data['title'] = 'Sambutan Kepala Sekolah';
-		
-		$this->db->from('profile');
-		$data['profile'] = $this->db->get()->row();
-
-		$this->load->view('sambutan',$data);
+		$data['profile'] = $this->Kategori_model->tampil_profile();
+		$this->template->load('template_detail','sambutan',$data);
 	}
 }
 ?>
