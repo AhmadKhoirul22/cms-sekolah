450c6f265339de8419229d6ec961b62bc263663b
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-05-18 12:29:53 +0700

    Update kategori.php

--- application/views/admin/kategori.php
+++ application/views/admin/kategori.php
@@ -45,30 +45,30 @@
 			<?php endif; ?>
 
 		</div>
-		<button type="button" class="btn btn-outline-primary block mb-3" data-bs-toggle="modal"
-			data-bs-target="#default">
+		<button type="button" class="btn btn-primary block mb-3" data-bs-toggle="modal" data-bs-target="#default">
 			Tambah Kategori
 		</button>
 		<!-- modal -->
-		<div class="modal fade text-left" id="default" tabindex="-1" aria-labelledby="myModalLabel1" aria-hidden="true"
-			style="display: none;">
+		<div class="modal fade text-left" id="default" tabindex="-1" aria-labelledby="myModalLabel1"
+			aria-hidden="true" style="display: none;">
 			<div class="modal-dialog modal-dialog-scrollable" role="document">
 				<div class="modal-content">
 					<div class="modal-header">
 						<h5 class="modal-title" id="myModalLabel1">Tambah Kategori</h5>
-						<button type="button" class="close rounded-pill" data-bs-dismiss="modal" aria-label="Close">
+						<button type="button" class="btn-close" data-bs-dismiss="modal"
+							aria-label="Close">
 							<i data-feather="x"></i>
 						</button>
 					</div>
 					<div class="modal-body">
-						<form action="<?= base_url('admin/kategori/tambah') ?>" method="post">
+						<form id="formTambahKategori" method="post">
 							<div class="mb-2">
 								<label for="" class="form-label">Nama Kategori</label>
 								<input type="text" name="nama_kategori" class="form-control" required>
 							</div>
 					</div>
 					<div class="modal-footer">
-						<button type="button" class="btn" data-bs-dismiss="modal">
+						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
 							<i class="bx bx-x d-block d-sm-none"></i>
 							<span class="d-none d-sm-block">Close</span>
 						</button>
@@ -82,7 +82,7 @@
 			</div>
 		</div>
 		<!-- end modal -->
-		<table class="table DataTable" id="table1">
+		<table class="table table-striped table-bordered" width="100%" id="datatable">
 			<thead>
 				<tr>
 					<th>No</th>
@@ -93,3 +93,201 @@
 		</table>
 	</div>
 </div>
+<!-- modal -->
+<div class="modal fade text-left" id="modalEdit" tabindex="-1" aria-labelledby="myModalLabel1"
+	aria-hidden="true" style="display: none;">
+	<div class="modal-dialog modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="myModalLabel1">Update Kategori</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+					<i data-feather="x"></i>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form id="formEditKategori" method="post">
+					<input type="hidden" name="id_kategori" id="id_kategori" value="">
+					<div class="mb-2">
+						<label for="" class="form-label">Nama Kategori</label>
+						<input type="text" name="nama_kategori" id="nama_kategori"
+							class="form-control" required>
+					</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+					<i class="bx bx-x d-block d-sm-none"></i>
+					<span class="d-none d-sm-block">Close</span>
+				</button>
+				<button type="submit" class="btn btn-primary ml-1">
+					<i class="bx bx-check d-block d-sm-none"></i>
+					<span class="d-none d-sm-block">Submit</span>
+				</button>
+			</div>
+			</form>
+		</div>
+	</div>
+</div>
+<!-- end modal -->
+<script>
+    $(document).ready(function () {
+        $('#datatable').DataTable({
+            "scrollX": true,
+            "ajax": {
+                "url": "<?= base_url('admin/kategori/getKategori') ?>",
+                "dataSrc": ""
+            },
+            "columns": [
+                {
+                    "render": function (data, type, full, meta) {
+                        return meta.row + 1; // Menampilkan nomor urut
+                    }
+                },
+                { "data": "nama_kategori" },
+                {
+                    "render": function (data, type, full, meta) {
+                        return `
+                            
+                                <button type="button" id="${full.id_kategori}" class="btn btn-primary" onclick="fun_edit(this.id)" title="Edit Kategori">
+                                    <i class="bi-pencil"></i>
+                                </button>
+                                <button type="button" id="${full.id_kategori}" class="btn btn-danger" onclick="fun_delete(this.id)" title="Hapus Kategori">
+                                    <i class="bi-trash"></i>
+                                </button>
+                            
+                        `;
+                    }
+                }
+            ]
+        });
+    });
+	function fun_edit(id_kategori) {
+		$('#modalEdit').modal('show');
+		$.getJSON('<?= base_url('admin/kategori/getKategoriID/') ?>'+id_kategori, function(json){
+			$('#nama_kategori').val(json.nama_kategori);
+			$('#id_kategori').val(json.id_kategori);
+		})
+	}
+
+	function fun_delete(id_kategori) {
+		var result = confirm("Yakin Hapus Kategori?");
+		if (result) {
+			$.ajax({
+				url: "<?= base_url('admin/kategori/delete/') ?>" + id_kategori,
+				dataType: "json",
+				type: "POST",
+				success: function (response) {
+					if (response.status === 'error') {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Data tidak valid',
+                    text: response.message,
+                    confirmButtonText: 'OK',
+                    timer: 3000
+                });
+            } else if (response.status === 'success') {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Berhasil',
+                    text: response.message,
+                    confirmButtonText: 'OK',
+                    timer: 3000
+                }).then(() => {
+                    $('#datatable').DataTable().ajax.reload(); 
+                });
+            }
+				},
+				error: function () {
+					Swal.fire({
+								icon: 'error',
+								title: 'Gagal!',
+								text: 'Terjadi kesalahan dalam update data.',
+								confirmButtonText: 'OK'
+							});
+				}
+			});
+		}
+	}
+
+	$('#formEditKategori').submit(function (e) {
+    e.preventDefault();
+    let formData = $(this).serialize();
+    $.ajax({
+        type: "POST",
+        url: "<?= base_url('admin/kategori/update') ?>",
+        data: formData,
+        dataType: "json",
+        success: function (response) {
+            if (response.status === 'error') {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Data tidak valid',
+                    text: response.message,
+                    confirmButtonText: 'OK',
+                    timer: 3000
+                });
+            } else if (response.status === 'success') {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Berhasil',
+                    text: response.message,
+                    confirmButtonText: 'OK',
+                    timer: 3000
+                }).then(() => {
+                    $('#modalEdit').modal('hide');
+                    $('#datatable').DataTable().ajax.reload(); 
+                });
+            }
+        },
+        error: function () {
+            Swal.fire({
+                icon: 'error',
+                title: 'Gagal!',
+                text: 'Terjadi kesalahan dalam update data.',
+                confirmButtonText: 'OK'
+            });
+        }
+    });
+});
+
+		// Menangani submit form tambah kategori
+		$('#formTambahKategori').submit(function(e) {
+			e.preventDefault();
+			let formData = $(this).serialize();
+			$.ajax({
+				type: "POST",
+				url: "<?= base_url('admin/kategori/tambah') ?>",
+				dataType: "json",
+				data: formData,
+				success: function(response) {
+					if (response.status === 'error') {
+						Swal.fire({
+							icon: 'error',
+							title: 'Data tidak valid',
+							text: response.message,
+							confirmButtonText: 'OK',
+							timer: 3000
+						});
+					} else if (response.status === 'success') {
+						Swal.fire({
+							icon: 'success',
+							title: 'Berhasil',
+							text: response.message,
+							confirmButtonText: 'OK',
+							timer: 3000
+						}).then(() => {
+							$('#default').modal('hide');
+							$('#datatable').DataTable().ajax.reload();
+						});
+            		}
+				},
+				error: function() {
+					Swal.fire({
+						icon: 'error',
+						title: 'Gagal!',
+						text: 'Terjadi kesalahan dalam update data.',
+						confirmButtonText: 'OK'
+					});
+				}
+			});
+		});
+</script>
