a47d4f03625ab2830c56c2d1226b1893fb13820f
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-05-18 12:27:32 +0700

    Update _css.php

--- application/views/admin/layout/_css.php
+++ application/views/admin/layout/_css.php
@@ -1,13 +1,10 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= $title ?></title>
-
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="<?= base_url('assets/mazer/dist/') ?>assets/css/bootstrap.css">
-
 <link rel="stylesheet" href="<?= base_url('assets/mazer/dist/') ?>assets/vendors/simple-datatables/style.css">
-
 <link rel="stylesheet"
 	href="<?= base_url('assets/mazer/dist/') ?>assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
 <link rel="stylesheet" href="<?= base_url('assets/mazer/dist/') ?>assets/vendors/bootstrap-icons/bootstrap-icons.css">
