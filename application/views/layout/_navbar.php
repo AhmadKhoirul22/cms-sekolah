7b32db5b06a52c61b17b83c2c041c39f45ef9a4e
AhmadKhoirul22 <slowmohuda@gmail.com>
2025-05-18 12:35:40 +0700

    update navbar and update js

--- application/views/layout/_navbar.php
+++ application/views/layout/_navbar.php
@@ -1,15 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
 	<a href="<?= base_url('home') ?>" class="navbar-brand p-0 mt-3">
-		<div class="row">
+		<div class="d-flex">
+			<img src="<?= base_url('assets/upload/icon/logo.png') ?>" alt="">
+			<h5 class="text-primary justify-content-start"></i><?=$profile->nama_profile ?></h5>
+		</div>
+		<!-- <div class="row">
 			<div class="col-3">
 				<img src="<?= base_url('assets/upload/icon/logo.png') ?>" alt="Logo">
 			</div>
-			<!-- <div class="col-2"></div> -->
+			<div class="col-2"></div>
 			<div class="col-9">
 				<h5 class="text-primary justify-content-start">SMP PENDA</h5>
 				<h5 class="text-primary justify-content-start">MOJOGEDANG</h5>
 			</div>
-		</div>
+		</div> -->
 	</a>
 	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
 		<span class="fa fa-bars"></span>
@@ -20,6 +24,8 @@
 		<div class="navbar-nav ms-auto py-0">
 			<a href="<?= base_url('home') ?>"
 				class="nav-item nav-link <?php if($menu == 'home'){echo 'active';} ?>">Beranda</a>
+				<a href="<?= base_url('ppdb_f') ?>"
+				class="nav-item nav-link <?php if($menu == 'ppdb_f'){echo 'active';} ?>">PPDB</a>
 			<div class="nav-item dropdown">
 				<a href="#" class="nav-link" data-bs-toggle="dropdown">
 					<span class="dropdown-toggle">Profile</span>
@@ -33,7 +39,7 @@
                                 <a href="404.html" class="dropdown-item">404 Page</a> -->
 				</div>
 			</div>
-			<a href="" class="nav-item nav-link">Artikel</a>
+			<a href="<?= base_url('artikel') ?>" class="nav-item nav-link <?php if($menu == 'artikel'){echo 'active';} ?>">Artikel</a>
 			<a href="<?= base_url('about') ?>"
 				class="nav-item nav-link <?php if($menu == 'about'){echo 'active';} ?>">Tentang</a>
 			<!-- <a href="blog.html" class="nav-item nav-link">Blogs</a>
